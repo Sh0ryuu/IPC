72aa203f6bed57ca428deefeded517623f3ceebe
OscarGJ4 <gallardojimenezoscar@gmail.com>
2024-05-15 23:40:01 +0200

    Add files via upload

---
+++ IPC_FXMLCore_CuentaDeGastos/src/javafxmlapplication/Utility.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafxmlapplication;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author alvar
+ */
+public class Utility {
+    
+    @FXML private BorderPane bp;
+    @FXML private AnchorPane ap;
+    
+    public static void changeScene(Class aClass, Event event, String sceneFileStr) throws Exception{
+        Parent root = FXMLLoader.load(aClass.getResource(sceneFileStr));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    
+}
