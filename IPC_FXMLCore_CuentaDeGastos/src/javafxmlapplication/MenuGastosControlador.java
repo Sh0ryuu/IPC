c80171dbfbf04bf302e6e323238ee1c85a9b35f7
OscarGJ4 <gallardojimenezoscar@gmail.com>
2024-05-17 12:29:28 +0200

    Add files via upload

---
+++ IPC_FXMLCore_CuentaDeGastos/src/javafxmlapplication/MenuGastosControlador.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafxmlapplication;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MenuGastosControlador {
+
+    @FXML
+    private Circle profilePicViewer;
+    @FXML
+    private ImageView profileImageView;
+    
+}
